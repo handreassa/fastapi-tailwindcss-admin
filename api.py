946490963040e49cc89456c05906c53c2f043895
Herivelton Andreassa <handreassa@gmail.com>
2023-08-13 21:41:13 -0300

    Updating home path

--- api.py
+++ api.py
@@ -16,8 +16,7 @@ class UnicornException(Exception):
 
 @app.get("/")
 async def root(request: Request):
-    return {"message": "Hello World"}
-    # return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse("home.html", {"request": request})
 
 @app.get("/chart")
 async def root(request: Request):
