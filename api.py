9cb6e80c0cb714f5a980405c82038c7c8313c489
Herivelton Andreassa <handreassa@gmail.com>
2024-06-27 21:16:11 -0300

    Refactoring to have tailwind templates working

--- api.py
+++ api.py
@@ -1,209 +1,141 @@
-from fastapi import FastAPI, Request, Depends, Form
-from fastapi.responses import HTMLResponse
-from fastapi import FastAPI, HTTPException, Request
+from datetime import datetime, timedelta, timezone
+from typing import Annotated, Optional, Union
+import jwt
+from fastapi import Depends, FastAPI, Form, HTTPException, Request, status
+from fastapi.responses import HTMLResponse,RedirectResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
 from fastapi.templating import Jinja2Templates
-from typing import Union, Optional
-import jwt
+from jwt.exceptions import DecodeError, ExpiredSignatureError, InvalidTokenError
 from passlib.context import CryptContext
-from datetime import datetime, timedelta
-from fastapi import FastAPI, Depends, HTTPException, Request
 from pydantic import BaseModel
-from jwt.exceptions import ExpiredSignatureError, InvalidTokenError, DecodeError
-
-
+import logging
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/src", StaticFiles(directory="src"), name="src")
 
+logger = logging.getLogger("uvicorn.error")
+logger.setLevel(logging.DEBUG)
+
 templates = Jinja2Templates(directory="templates")
 
-# Secret key for JWT
-SECRET_KEY = "your_secret_key"
+# to get a secret key string like this example, run this command on terminal:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-# Sample user data (to be replaced with user database)
+# Sample user data (this is a placeholder to be replaced with user database)
 users_db = {
-    "user1": {
-        "username": "user1@test.com",
-        "password_hash": "$2b$12$MhvChXs.Zuk8z3jwTNMCiOt2nvw0.cToC7kJ5YrA0LOrgktJx2IQy",  # Password: password1
+    "placeholder123": {
+        "username": "placeholder123",
+        "email": "test@example.com",
+        "password_hash": "$2b$12$ugtN8kcm5z/dAMAx7faQkeny9Y.PgYZnQJX/VJc7K/DfdgrVJpwEa",  # Password: password1
     }
 }
 
-
-class User(BaseModel):
-    username: str
-
-
-class UserInDB(User):
-    password_hash: str
-
+class UserInDB:
+    def __init__(self, username, email, password_hash):
+        self.username = username
+        self.email = email
+        self.password_hash = password_hash
 
 class Token(BaseModel):
     access_token: str
     token_type: str
 
-
 class TokenData(BaseModel):
     username: Union[str, None] = None
 
-
-class UnicornException(Exception):
-    def __init__(self, name: str):
-        self.name = name
-
-
-# Password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-# OAuth2 password flow
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-
-# Generate JWT token
-def create_access_token(data: dict, expires_delta: timedelta = timedelta(hours=1)):
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + expires_delta
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+def verify_password(plain_password, password_hash):
+    return pwd_context.verify(plain_password, password_hash)
 
-# Verify user credentials
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-# Get user by username
-def get_user(username: str):
-    if username in users_db:
-        user_dict = users_db[username]
+def get_user(db, username: str):
+    if username in db:
+        # logger.debug(f"Username: {username}")
+        user_dict = db[username]
         return UserInDB(**user_dict)
+    # else:
+    #     logger.debug(f"Username not found: {username}")
 
-
-# Authenticate user
-def authenticate_user(username: str, password: str):
-    user = get_user(username)
+def authenticate_user(db, username: str, password: str):
+    user = get_user(db, username)
+    logger.debug(f"User: {user}")
     if not user:
         return False
     if not verify_password(password, user.password_hash):
         return False
     return user
 
-
-# Create a token route
-@app.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(status_code=401, detail="Incorrect username or password")
-    access_token = create_access_token(data={"sub": user.username})
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-@app.get("/")
-async def root(request: Request):
-    return templates.TemplateResponse("landing_page.html", {"request": request})
-
-
-@app.exception_handler(HTTPException)
-async def http_exception_handler(request: Request, exc: HTTPException):
-    return templates.TemplateResponse(
-        "exception.html", {"request": request, "error": exc}
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
     )
-
-
-def get_current_user(token: str = Depends(oauth2_scheme)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
+        logger.debug(f"Username: {username}")
         if username is None:
-            raise HTTPException(
-                status_code=401, detail="Could not validate credentials"
-            )
+            logger.debug("Username is None")
+            raise credentials_exception
         token_data = TokenData(username=username)
-    except jwt.ExpiredSignatureError:
-        raise HTTPException(status_code=401, detail="Token has expired")
-    except jwt.DecodeError:
-        raise HTTPException(status_code=401, detail="Could not validate credentials")
-    return token_data
+        logger.debug(f"token_data: {token_data}")
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
 
 
-# Middleware to check for authentication
-@app.middleware("http")
-async def check_authentication(request: Request, call_next):
-    if (
-        not request.url.path.startswith("/login")
-        and not request.url.path.startswith("/password-reset")
-        and request.url.path != "/"
-    ):
-        # Redirect unauthenticated users to the login page
-        try:
-            token = request.cookies.get("access_token")
-            get_current_user(token)  # Validate the token
-        except ExpiredSignatureError:
-            response = templates.TemplateResponse(
-                "login.html", {"request": request, "message": "Token has expired"}
-            )
-            response.status_code = 401
-            return response
-        except DecodeError:
-            response = templates.TemplateResponse(
-                "login.html", {"request": request, "message": "Invalid token"}
-            )
-            response.status_code = 401
-            return response
-    response = await call_next(request)
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+)-> Token:
+    user = authenticate_user(users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    # return Token(access_token=access_token, token_type="bearer")
+    response = RedirectResponse(url="/charts", status_code=status.HTTP_303_SEE_OTHER)
+    response.set_cookie(key="access_token", value=f"Bearer {access_token}", httponly=True)
     return response
-
 
 @app.get("/login", response_class=HTMLResponse)
 async def login_form(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
-
-@app.post("/login", response_class=HTMLResponse)
-async def login_submit(
-    request: Request, username: str = Form(...), password: str = Form(...)
-):
-    user = authenticate_user(username, password)
-    if not user:
-        # Authentication failed
-        return templates.TemplateResponse(
-            "login.html",
-            {"request": request, "message": "Incorrect username or password"},
-        )
-
-    # Authentication successful, generate a JWT token
-    access_token = create_access_token(data={"sub": user.username})
-    return templates.TemplateResponse(
-        "login.html",
-        {"request": request, "message": "Login successful", "token": access_token},
-    )
-
-
-@app.get("/register", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse("register.html", {"request": request})
-
-
-# Create a new POST endpoint for user registration
 @app.post("/register", response_class=HTMLResponse)
 async def register(
     request: Request, username: str = Form(...), password: str = Form(...)
 ):
-    # Check if the username is already registered (you should implement your own logic here)
     if username in users_db:
         return templates.TemplateResponse(
             "register.html", {"request": request, "message": "Username already exists"}
         )
-
-    # Hash the user's password (you should use a more secure method for production)
     password_hash = pwd_context.hash(password)
-
-    # Store the user data (in-memory, replace with your own database logic)
     users_db[username] = {"username": username, "password_hash": password_hash}
 
     response = templates.TemplateResponse(
@@ -212,11 +144,9 @@ async def register(
     response.set_cookie("flash_message", "Registration successful")
     return response
 
-
 @app.get("/password-reset", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("password-reset.html", {"request": request})
-
 
 @app.post("/password-reset", response_class=HTMLResponse)
 async def password_reset(
@@ -238,3 +168,23 @@ async def password_reset(
     )
     response.set_cookie("flash_message", "Password reset successful")
     return response
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    return templates.TemplateResponse(
+        "exception.html", {"request": request, "error": exc}
+    )
+# Landing page
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("landing_page.html", {"request": request})
+
+# Protected dashboard
+# @app.get("/chart", response_class=HTMLResponse)
+# def charts(request: Request, current_user: TokenData = Depends(get_current_user)):
+#     return templates.TemplateResponse("chart2.html", {"request": request, "user": current_user})
+
+
+@app.get("/dashboard", response_class=HTMLResponse)
+def dash(request: Request):
+    return templates.TemplateResponse("dashboard.html", {"request": request})
--- tailwind.config.js
+++ tailwind.config.js
@@ -31,52 +31,41 @@ module.exports = {
   // enable dark mode via class strategy
   darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        primary: {"50":"#ecfeff","100":"#cffafe","200":"#a5f3fc","300":"#67e8f9","400":"#22d3ee","500":"#06b6d4","600":"#0891b2","700":"#0e7490","800":"#155e75","900":"#164e63"}
-      }
-    },
     fontFamily: {
-      'body': [
-    'Inter', 
-    'ui-sans-serif', 
-    'system-ui', 
-    '-apple-system', 
-    'system-ui', 
-    'Segoe UI', 
-    'Roboto', 
-    'Helvetica Neue', 
-    'Arial', 
-    'Noto Sans', 
-    'sans-serif', 
-    'Apple Color Emoji', 
-    'Segoe UI Emoji', 
-    'Segoe UI Symbol', 
-    'Noto Color Emoji'
-  ],
-      'sans': [
-    'Inter', 
-    'ui-sans-serif', 
-    'system-ui', 
-    '-apple-system', 
-    'system-ui', 
-    'Segoe UI', 
-    'Roboto', 
-    'Helvetica Neue', 
-    'Arial', 
-    'Noto Sans', 
-    'sans-serif', 
-    'Apple Color Emoji', 
-    'Segoe UI Emoji', 
-    'Segoe UI Symbol', 
-    'Noto Color Emoji'
-  ]
-    }
+      display: ['Roboto Mono', 'Menlo', 'monospace'],
+      body: ['Roboto Mono', 'Menlo', 'monospace'],
+    },
+    colors: {
+      primary: {
+        50: '#f7fee7',
+        100: '#ecfccb',
+        200: '#d9f99d',
+        300: '#bef264',
+        400: '#a3e635',
+        500: '#84cc16',
+        600: '#65a30d',
+        700: '#4d7c0f',
+        800: '#3f6212',
+        900: '#365314',
+      },
+      gray: {
+        50: '#fafafa',
+        100: '#f4f4f5',
+        200: '#e4e4e7',
+        300: '#d4d4d8',
+        400: '#a1a1aa',
+        500: '#71717a',
+        600: '#52525b',
+        700: '#3f3f46',
+        800: '#27272a',
+        900: '#18181b',
+      },
+    },
   
   },
   plugins: [
     // include Flowbite as a plugin in your Tailwind CSS project
-    // require('flowbite/plugin')
+    require('flowbite/plugin')
   ],
   DARK_CLASS_NAME,
   DARK_COLOR_THEME,
